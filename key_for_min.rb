764ca91527437e3aed82b598e84a5d6571e68166
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-20 16:22:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_hash_key == nil
       return smallest_hash_key = key
+      next
     elsif val < name_hash[smallest_hash_key]
       smallest_hash_key = key
     end
